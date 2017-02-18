54f12e995a18bb7ccd26d043d12e5cc836773fd4
vojtechnovy <yotis@centrum.cz>
2017-02-18 14:35:48 +0100

    Create SOT-DL.py

---
+++ SOT-DL.py
@@ -0,0 +1,11 @@
+import webbrowser
+import time
+
+new = 2
+
+words = ["%EC%8B%9C%EA%B0%84", "%EC%96%B4%EA%B9%A8", "%EC%95%84%ED%8C%8C", "Hello", "Just", "an", "Example"] # replace this list with your own words
+
+for i in words:
+    time.sleep(0.1)     # delay for 0.1 sec, should not be necessary
+    url = "http://soundoftext.com/static/sounds/ko/%s.mp3" % i  # replace "ko" with language of your choice
+    webbrowser.open(url, new=new)
