8061734fe72093c4d766a986a2d37f08b3415c50
vojtechnovy <yotis@centrum.cz>
2017-02-25 12:11:07 +0100

    Update SOT-Downloader.py

--- SOT-Downloader.py
+++ SOT-Downloader.py
@@ -1,11 +1,36 @@
+# -*- coding: utf-8 -*-
 import webbrowser
 import time
+import urllib
 
+
+# path to your txt file here with double "\\" 
+word_file = 'C:\\Users\\antonin\\Desktop\\book2.txt' 
+
+
+# see browser opening 
 new = 2
+# see Reading source file
+lines = []
+# see url code conversion
+words_url = []
 
-words = ["%EC%8B%9C%EA%B0%84", "%EC%96%B4%EA%B9%A8", "%EC%95%84%ED%8C%8C", "Hello", "Just", "an", "Example"] # replace this list with your own words
 
-for i in words:
+# Reading source file by lines and adding it to a list, here UTF is useless and we want to read as binary 'rb'
+with open(word_file, 'rb') as f:
+    lines = f.readlines()
+    lines = [x.strip() for x in lines] # you may also want to remove whitespace characters like `\n` at the end of each line
+
+    
+# we take a list, convert it to url code and save it into a new list
+for word in lines:
+    word_url = urllib.parse.quote_plus(word)
+    words_url.append(word_url)
+
+print(words_url) 
+
+# Opens a link in new window in your browser for each word in the list
+for i in words_url:
     time.sleep(0.1)     # delay each request for 0.1 sec, should not be necessary
     url = "http://soundoftext.com/static/sounds/ko/%s.mp3" % i  # replace "ko" with language of your choice!!
     webbrowser.open(url, new=new)
